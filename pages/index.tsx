60b84d97669abec530ecd6fa1624222048f469e8
qatta93 <panasiuk.patrycja@gmail.com>
2022-06-20 21:32:34 +0200

    database is rendering correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,41 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button';
 import { PrismaClient } from '@prisma/client';
+import { useState } from 'react';
 
 const prisma = new PrismaClient();
 
 interface TodoListProps {
   todoListId: number,
-  todoListName: string
+  todoListName: string,
 }
 
- // @ts-ignore:next-line
-const Home: Promise<any> = async () => {
+interface TodoListArrayProps {
+  todoList: TodoListProps[],
+}
+
+export const getServerSideProps = async () => {
   const todoList = await prisma.todoList.findMany();
   console.log(todoList)
+  return {
+    props: {
+      initialList: todoList
+    }
+  };
+}
 
+const Home: NextPage =  ({ initialList }:any ) => {
+  const [list, setList] = useState(initialList)
   return (
     <div className={styles.home__container}>
       <section className={styles.home__intro}>
         <img src="img/bcg.png" alt="todo-list" />
-        <p>{todoList}</p>
         <p>Do you know that having TODO LIST can potentially sabotage your productivity?</p>
         <Button variant="contained">CREATE NEW LIST</Button>
       </section>
       <section className={styles.home__lists}>
         <h1 className={styles.home__listsTitle}>TODO LISTS:</h1>
-        {todoList.map((todo:TodoListProps) => <Button variant="outlined" className={styles.home__listsBtn}>{todo.todoListName}</Button>)}
+        {list.map((todo:TodoListProps) => <Button variant="outlined" className={styles.home__listsBtn}>{todo.todoListName}</Button>)}
       </section>
     </div>
   )
